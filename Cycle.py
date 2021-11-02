15b016a3365638245f95d963334155bc41bd74cf
Malyshka <krautsovasviatlana@gmail.com>
2021-11-02 22:02:57 +0100

    add changes

--- Cycle.py
+++ Cycle.py
@@ -1,7 +1,12 @@
 import time
 count = 0
-while True:
-    print(count, "- Hello")
-    count +=1
-    time.sleep(.200)
-    break
+# while True:
+#     print(count, "- Hello")
+#     count +=1
+#     time.sleep(.200)
+#     break
+
+while count < 10:
+    count += 1
+    print(count, "-Hello")
+    time.sleep(.500)
