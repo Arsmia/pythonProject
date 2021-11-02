2b4757d85cadf6a27a5838005104ce92b986c128
Malyshka <krautsovasviatlana@gmail.com>
2021-11-02 22:11:38 +0100

    add Hw_4

---
+++ HW_4.1,4.2.py
@@ -0,0 +1,27 @@
+# # Task № 1
+# def convert_currency(real_to_USD):
+while True:
+
+    amount = input("enter BYN ")
+    conversion_result = int(amount) / 2.55
+    print(conversion_result)
+    print("Ты ввёл " + amount + " USD" + ", конвертируемая сумма в " + "BYN " + " = " + str(conversion_result))
+
+
+# Task № 2
+def convert_currency(real_from_BYN):
+count = 0
+amount = input("Enter BYN ")
+currency_usd = 2.55
+currency_eur = 2.92
+currency_chf = 2.67
+currency_gbp = 3.42
+currency_cny = 0.38
+conversion_result = ([int(amount) / currency_usd], [int(amount) / currency_eur],[int(amount) / currency_chf], [int(amount) / currency_gbp], [int(amount) / currency_cny])
+print("conversion_result_usd == ", conversion_result[0])
+print("conversion_result_eur == ", conversion_result[1])
+print("conversion_result_chf == ", conversion_result[2])
+print("conversion_result_gbp == ", conversion_result[3])
+print("conversion_result_cny == ", conversion_result[4])
+print("Press Enter to exit")
+break
---
+++ HW_4.3.py
@@ -0,0 +1,59 @@
+Задача №3
+# Обменник. Создать скрипт который будет запускаться из консоли 1 раз из консоли, выдавать результат и зарываться.
+#     1. На вход обменнику вводишь количество денег int.
+#     2. На выходе в консоль выводится отввет в таком виде:
+#                 "Ты ввёл int (Валюта)"
+#                 "конвертированная сумма в USD = int"
+#                 "конвертированная сумма в EUR = int"
+#                 "конвертированная сумма в CHF = int"
+#                 "конвертированная сумма в GBP = int"
+#                 "конвертированная сумма в CNY = int"
+#
+#     3. Скипт должен выдать сообщение
+#                 "Введите положительное число." Если число меньше 0.
+#                 "Вы ввели не число. Введите число." Если введены буквы.
+#                 "Вы ввели пустое поле. Введите число." Если введено пустое значение.
+#     4. Валюту пользователя определите сами.
+
+def main():
+   your_money = get_money()
+   your_currency = get_currency(str(your_money))
+   print("your_money = ", your_currency)
+
+
+def get_money():
+    while True:
+        your_money = input("Enter your money: ")
+        if your_money is " ":
+            print("Вы ввели пустое поле. Введите число.")
+            return your_money, False
+        if int(your_money) <= 0:
+            print("Введите положительное число.")
+            return your_money, False
+        try:
+            your_money = int(your_money)
+        except ValueError:
+            print("Вы ввели не число. Введите число.")
+        if int(your_money) > 0:
+            return your_money, True
+        continue
+
+def get_currency(your_money):
+    int_money = int(your_money)
+    return int_money
+currency_list = ["USD","EUR","CHF","GBP","CNY"]
+your_money = input("Enter your money: ")
+currency_usd = 2.55
+currency_eur = 2.92
+currency_chf = 2.67
+currency_gbp = 3.42
+currency_cny = 0.38
+conversion_result = ([int(your_money) / currency_usd], [int(your_money) / currency_eur], [int(your_money) / currency_chf], [int(your_money) / currency_gbp], [int(your_money) / currency_cny])
+print("conversion_result_usd == ", conversion_result[0])
+print("conversion_result_eur == ", conversion_result[1])
+print("conversion_result_chf == ", conversion_result[2])
+print("conversion_result_gbp == ", conversion_result[3])
+print("conversion_result_cny == ", conversion_result[4])
+
+# while True:
+# main()
